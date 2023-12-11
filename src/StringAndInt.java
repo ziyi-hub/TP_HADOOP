4bcb1d9cdec809cea9b325516adc52ab69bceb81
Ziyi WANG <ziyiwang1027@gmail.com>
2023-12-12 00:33:31 +0100

    Add Q19

--- src/StringAndInt.java
+++ src/StringAndInt.java
@@ -1,8 +1,16 @@
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
-public class StringAndInt implements Comparable<StringAndInt> {
+import org.apache.hadoop.io.Writable;
+
+public class StringAndInt implements Comparable<StringAndInt>, Writable {
 	
 	private String tag;
 	private Integer occurrences;
+	
+	public StringAndInt() {
+    }
 	
 	StringAndInt(String tag, Integer occ){
 		this.tag = tag;
@@ -22,6 +30,18 @@ public class StringAndInt implements Comparable<StringAndInt> {
 	@Override
 	public int compareTo(StringAndInt other) {
 		return -Integer.compare(this.occurrences, other.occurrences);
+	}
+
+	@Override
+	public void readFields(DataInput arg0) throws IOException {
+		tag = arg0.readUTF();
+        occurrences = arg0.readInt();
+	}
+
+	@Override
+	public void write(DataOutput arg0) throws IOException {
+		arg0.writeUTF(tag);
+		arg0.writeInt(occurrences);
 	}
 	
 }
